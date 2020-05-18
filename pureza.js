d43f955ccc18fb6711e3f982a1a42b22ee327679
cuongnguyend <52162892+cuongnguyend@users.noreply.github.com>
2020-05-18 20:25:56 +0700

    cuong commit dau tien

--- pureza.js
+++ pureza.js
@@ -20,6 +20,7 @@ $('.slide').mousemove(function(event){
     $('.carousel-caption').css('margin-left', moveX + 'px');
     $('.carousel-caption').css('margin-top', moveY + 'px');
 });
+
 // slide
 // backtop
 $(document).ready(function() {
@@ -40,4 +41,38 @@ $(document).ready(function() {
     });
 // backtop
 
+// sticky navbar
+$(document).ready(function() {
+    // Custom 
+    var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
+      var stickyHeight = sticky.outerHeight();
+      var stickyTop = stickyWrapper.offset().top;
+      if (scrollElement.scrollTop() >= stickyTop){
+        stickyWrapper.height(stickyHeight);
+        sticky.addClass("is-sticky");
+      }
+      else{
+        sticky.removeClass("is-sticky");
+        stickyWrapper.height('auto');
+      }
+    };
+    
+
+    $('[data-toggle="sticky-onscroll"]').each(function() {
+      var sticky = $(this);
+      var stickyWrapper = $('<div>').addClass('sticky-wrapper'); 
+      sticky.before(stickyWrapper);
+      sticky.addClass('sticky');
+      
+      // Scroll & resize events
+      $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
+        stickyToggle(sticky, stickyWrapper, $(this));
+      });
+      
+      // On page load
+      stickyToggle(sticky, stickyWrapper, $(window));
+    });
+  });
+
+// sticky navbar
 
